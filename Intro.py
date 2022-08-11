64a01b02baa14a49b50820b92a6561381a84dabe
Chinmay Kulkarni <ckulkar1@tlu.ee>
2022-08-11 16:25:05 +0300

    Intro.py created

---
+++ Intro.py
@@ -0,0 +1,71 @@
+print(2+2)
+
+"""
+Haa haa haaaaaa!
+"""
+
+comment = "You are "
+human = "human"
+
+print(comment + human)
+
+comp = "1"
+
+print(type(comp))
+
+comp = int(comp)
+print(comp)
+
+print(2<=1)
+
+exp = 2 ** 4
+
+print(exp)
+
+print(10//9)
+
+print(pow(2, 4))
+
+hello = "Welcome to python"
+
+#for char in hello:
+ #   print(char)
+
+txt = "It's a beautiful day"
+print("beau" in txt)
+
+print(txt[3:4])
+print(txt[-4:-1])
+
+i = 0
+while i < len(txt):
+    print(txt[i])
+    i += 2
+
+age = "10"
+name = "chinmay"
+
+
+print("My name is " + name + "My age is " + age)
+
+myList = [0,1,2,3,4,4,5,6,6,6,7]
+
+for var in myList:
+    print("I am a number " + str(var))
+
+ages = [12, 25, 47, 55, 64, 23]
+
+
+#for i in range(len(str(ages))):
+ #   print("index: " + str(i) + "age " + str(age[i]))
+
+mySet = {'mangoes', 'grapes', 'apples', 'kiwis'}
+print(mySet)
+
+list = [1,2,3,4,5]
+print(type(list))
+
+
+
+
+
